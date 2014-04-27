8ef09f39f5f4850973ef005f53b8444e2dbdc11b
santosh kumar singh <santosh.ksingh03@gmail.com>
2014-04-27 19:54:58 +0530

    initial commit

---
+++ FacebookFrinedConnect/Controllers/HomeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace FacebookFriendshipCalculator
+{
+
+    public class Data
+    {
+        public string Text { get; set; }
+        public string Name { get; set; }
+
+    }
+    public class Probability
+    {
+        public double neg { get; set; }
+        public double neutral { get; set; }
+        public double pos { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Probability probability { get; set; }
+        public string label { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        //A Sample void function to send form data
+        public string SubmitFormData(string url, string data)
+        {
+            // Create web request object
+            WebRequest objWebRequest;
+
+            // Set url properties
+            // string url = "http://localhost/sampleform/response.aspx";
+            objWebRequest = WebRequest.Create(url);
+            objWebRequest.Method = "POST";
+
+            // add sample form data
+            ArrayList queryList = new ArrayList();
+            queryList.Add(string.Format("{0}={1}", "value1", "text1"));
+
+
+            // Set the encoding type
+            objWebRequest.ContentType = "application/x-www-form-urlencoded";
+
+            objWebRequest.ContentLength = data.Length;
+
+            // Write stream
+            StreamWriter sw = new StreamWriter(objWebRequest.GetRequestStream());
+            sw.Write(data);
+            sw.Close();
+
+            //we get back the response after submission
+            HttpWebResponse objHttpWebResponse;
+            objHttpWebResponse = (HttpWebResponse)objWebRequest.GetResponse();
+            StreamReader sr = new StreamReader(objHttpWebResponse.GetResponseStream());
+
+            //we can print this out to see the response result
+            //SEE BELOW RESPONSE.ASPX on HOW TO READ THE FORM DATA
+            return sr.ReadToEnd();
+        }
+        private static string UrlEncode(IDictionary<string, string> parameters)
+        {
+            var sb = new StringBuilder();
+            foreach (var val in parameters)
+            {
+                // add each parameter to the query string, url-encoding the value.
+                sb.AppendFormat("{0}={1}&", val.Key, HttpUtility.UrlEncode(val.Value));
+            }
+            sb.Remove(sb.Length - 1, 1); // remove last '&'
+            return sb.ToString();
+        }
+        public JsonResult Sentiment(Data data)
+        {
+            var url = "http://text-processing.com/api/sentiment/";
+            //# when we pass text in as post request to api, we cannot have any unicode chars
+            //var text = request.POST['text'].encode('ascii', 'ignore');
+            //# sentiment analysis api limits to 10000 characters, 9900 to be safe
+            //if len(text) >= 9900:
+            //	text = text[:9900]
+            //var values = "{'text' : " + data.Text + "}";
+            var text = data.Text;
+            var parameters = new Dictionary<string, string> 
+                                { 
+                                  {"text" , text}
+                                };
+            var values = UrlEncode(parameters);
+            var res = SubmitFormData(url, values);
+            var x = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(res);
+
+            //data = urllib.urlencode(values)
+            //req = urllib2.Request(url, data)
+            //response = urllib2.urlopen(req)
+            //sentiment = json.loads(response.read())
+            //sentiment = round(sentiment['probability']['pos'] * 100)
+            var sentiment = (int)Math.Round(x.probability.pos * 100);
+            var name = data.Name;
+            var message = "adafd'";
+            if (sentiment <= 10)
+                message = "Wow.. forever alone";
+            else if (10 < sentiment && sentiment <= 20)
+                message = "Basically, you're hated";
+            else if (20 < sentiment && sentiment <= 30)
+                message = "Friend-zoned";
+            else if (30 < sentiment && sentiment <= 40)
+                message = "Uhh good luck?";
+            else if (40 < sentiment && sentiment < 50)
+                message = "So close, but so far.";
+            else if (sentiment == 50)
+                message = "Love-hate relationship?";
+            else if (50 < sentiment && sentiment <= 60)
+                message = "You've got a chance!";
+            else if (60 < sentiment && sentiment <= 70)
+                message = "Make a move already!";
+            else if (70 < sentiment && sentiment <= 80)
+                message = "If you're not dating, you should be.";
+            else if (80 < sentiment && sentiment <= 90)
+                message = "I bet you're only doing this to make your friends jealous.";
+            else if (90 < sentiment)
+                message = "Wtf just go get a room already";
+            var result = new { sentiment = sentiment, name = data.Name, message = message };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FacebookFrinedConnect/Scripts/main.js
@@ -0,0 +1,231 @@
+$(document).ready(function () {
+    // using jQuery
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    // set csrf token as a header in ajax request
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+    // images
+    $authImg = $("#auth-img");
+    $findFriendImg = $("#find-friend-img");
+    $shareImg = $('#share-img');
+
+    $message = $('#message');
+
+    function activateFriendSelctor() {
+        $findFriendImg.fSelector({
+            onSubmit: function (selectedFriendIds) {
+                if (selectedFriendIds.length > 0) {
+                    // get name of romantic interest and create dict romInterest w/ id & name
+                    FB.api('/' + selectedFriendIds[0], function (response) {
+                        romInterest = { 'id': selectedFriendIds[0], 'name': response.name };
+
+                        query1 = encodeURIComponent('SELECT thread_id FROM thread WHERE folder_id=0')
+                        query = encodeURIComponent('SELECT body FROM message WHERE thread_id IN (SELECT thread_id FROM thread WHERE folder_id=0) AND author_id=' + romInterest.id);
+                        FB.api('/fql?q=' + query, function (response) {
+                            if (!response || response.error) {
+                                $message.html("Sorry, Facebook says you've maxed out on your tries!  Please try again in 5 minutes.")
+                            } else {
+                                handleConversations(response.data);
+                            }
+                        })
+                    });
+
+                    // get conversation data of selectedfriend
+                    /*FB.api('/me/inbox', {limit:20}, function(response){
+                      if (!response || response.error){
+                        $message.html("Sorry, Facebook says you've maxed out on your tries!  Please try again in 5 minutes.")
+                      } else{
+                        filterConversations(response);
+                      }
+                    });*/
+
+                } else {
+                    $message.html('Please select someone!')
+                }
+            },
+            facebookInvite: false,
+            closeOnSubmit: true,
+            max: 1,
+            showButtonSelectAll: false,
+            showSelectedCount: true,
+            lang: {
+                title: "Select a friend",
+                buttonSubmit: "OK"
+            }
+        });
+    }
+
+    function handleConversations(msgs) {
+        if (msgs.length == 0) {
+
+            $message.html(romInterest.name + " hasn't talked to you in forever!  Maybe you should do something about that.  Try picking someone else.");
+
+        } else {
+
+            var text = concatenateMessages(msgs);
+
+            // remove handler on find-friend
+            $findFriendImg.off();
+
+            // hide choose-friend img to show checkmark bg
+            $findFriendImg.animate({ opacity: 0 });
+            $findFriendImg.css({
+                visibility: 'hidden',
+                cursor: 'default'
+            });
+            $("#percentage").removeClass('black');
+
+            // add user name to data
+            data = { text: text, name: romInterest.name };
+            // ajax to get sentiment value of text
+            $.ajax({
+                type: "POST",
+                url: "/Home/Sentiment",
+                data: data,
+                success: function (data) {
+                    //                    alert(data.sentiment);
+                    // animate sentiment percentage update
+                    //                  data = $.parseJSON(data);
+                    console.log(data);
+                    var $sentiment = $('#sentiment');
+                    var currentVal = $sentiment.text();
+                    var endVal = data.sentiment;
+                    var updatePercentage = setInterval(function () {
+                        if (currentVal == endVal) {
+                            clearInterval(updatePercentage);
+                            $('#share').removeClass('black');
+
+                            var imgUrl = 'http://' + window.location.hostname + '/static/img/logo.png'
+
+                            $shareImg.click(function (e) {
+                                FB.ui({
+                                    method: 'feed',
+                                    link: 'http://aspdotnetcodebook.blogspot.com/',
+                                    name: data.name + ' has a ' + endVal + '% romantic interest in me!',
+                                    caption: data.message,
+                                    picture: imgUrl,
+                                    description: 'Ever wondered how your romantic interest feels about you?  Is She Into Me runs sentiment analysis on your conversations with a friend to give you an interest score.'
+                                }, function (response) {
+                                    if (response && response.post_id) {
+                                        $shareImg.animate({ opacity: 0 });
+                                        $shareImg.css({
+                                            visibility: 'hidden',
+                                            cursor: 'default'
+                                        });
+                                        $("#share").removeClass('black');
+                                        $shareImg.off();
+                                    }
+                                });
+                            });
+
+                            $message.html(data.message);
+                        } else {
+                            currentVal++;
+                            $sentiment.text(currentVal);
+                        }
+                    }, 100);
+                }
+            });
+        }
+    }
+
+    function concatenateMessages(convos) {
+        text = '';
+        for (var i = 0; i < convos.length; i++) {
+            text += convos[i].body + ' ';
+        }
+        return text;
+    }
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '524264804350512',
+            status: true,
+            cookie: true,
+            xfbml: true,
+            oauth: true
+        });
+
+        function updateButton(response) {
+
+            if (response.authResponse) {
+                // hide auth img to show checkmark bg
+                $authImg.animate({ opacity: 0 });
+                $authImg.css({
+                    visibility: 'hidden',
+                    cursor: 'default'
+                });
+                $("#find-friend").removeClass('black');
+                activateFriendSelctor();
+
+            } else {
+                //user is not connected to your app or logged out
+                $authImg.click(function (e) {
+                    e.preventDefault();
+                    FB.login(function (response) {
+                        if (response.authResponse) {
+                            // hide choose-friend img to show checkmark bg
+                            $authImg.animate({ opacity: 0 });
+                            $authImg.css({
+                                visibility: 'hidden',
+                                cursor: 'default'
+                            });
+                            $("#find-friend").removeClass('black');
+
+                            // remove handler for logging in
+                            $authImg.off();
+
+                            $findFriendImg.click(function (e) {
+                                e.preventDefault();
+                                activateFriendSelctor();
+                            });
+                        } else {
+                            //user cancelled login or did not grant authorization
+                        }
+                    }, { scope: 'read_mailbox' });
+                });
+
+            }
+        }
+
+        // run once with current status and whenever the status changes
+        FB.getLoginStatus(updateButton);
+        FB.Event.subscribe('auth.statusChange', updateButton);
+    };
+
+    (function () {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol
+          + '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+    }());
+
+});
